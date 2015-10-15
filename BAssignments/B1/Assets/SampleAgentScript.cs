63fa1f26f13ab58a9385f72f18ec71439fb0f455
ericsong <regonics@gmail.com>
2015-10-15 12:02:02 -0400

    init commit with half of part 1

---
+++ BAssignments/B1/Assets/SampleAgentScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SampleAgentScript : MonoBehaviour {
+	public Camera camera;
+	public Transform target;
+	NavMeshAgent agent;
+		
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			RaycastHit hit;
+			Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+			
+			if (Physics.Raycast(ray, out hit)) {
+				Debug.Log (hit.point);
+				agent.SetDestination(hit.point);
+			}
+		}
+	}
+}
